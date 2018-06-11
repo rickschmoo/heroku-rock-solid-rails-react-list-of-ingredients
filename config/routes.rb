1e01876e1de7737d3e6d57c28f589e7093319938
Richard Boardman <rickschmoo@gmail.com>
2018-06-10 21:51:30 -0700

    React router

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
     resources :drinks
   end
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get '*path', to: "application#fallback_index_html",
+		constraints: ->(request) do
+	  		!request.xhr? && request.format.html?
+	end
+
 end
