83396085ed6eb933744fc79cb18c172e5713dae5
Richard Boardman <rickschmoo@gmail.com>
2018-06-10 21:21:06 -0700

    Simple client

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    window.fetch('/api/drinks')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error));
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>Welcome to React</h2>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
