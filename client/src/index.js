440633c63bfc0f85f702a345db7843260c02758d
Richard Boardman <rickschmoo@gmail.com>
2018-06-10 21:32:45 -0700

    Swish client using semantic ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import 'semantic-ui-css/semantic.css'
+import './index.css'
 // import registerServiceWorker from './registerServiceWorker';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
